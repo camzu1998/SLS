649680c0ec9b5ca9eb2c2a1f9c6d7cdc8e23ca38
camzu1998 <kamillanger4@gmail.com>
2017-12-06 16:56:50 +0100

    Lista Sezonow FIX

--- listaSezonow.php
+++ listaSezonow.php
@@ -7,23 +7,42 @@ require_once "connect.php";
 if($polaczenie->connect_errno!=0){
     echo "Error: ".$polaczenie->connect_errno;
 } else {
-    $ileSez = $polaczenie->query("SELECT * FROM `sezony` WHERE `Zakonczony` != 0;");
-    $aktywny = $polaczenie->query("SELECT * FROM `sezony` WHERE `Zakonczony` = 0;");
-    $wiersz = $aktywny->fetch_assoc();
-        $Nazwa = $wiersz['Data'];
-        $ID = $wiersz['ID'];
-        $Zakonczony = $wiersz['Zakonczony'];
-    echo '<div class="menuSez">';
-        echo '<a class="first" href="index.php?id='.$ID.'">'.$Nazwa.'</a>';
-        echo '<div class="menuContent">';
-        for($i=0;$i<$ileSez->num_rows;$i++){
-            $wiersz = $ileSez->fetch_assoc();
-            $ID = $wiersz['ID'];
+    @$IdSezonu = @$_GET['id'];
+    if(@$IdSezonu != null){
+        $wybrany = $polaczenie->query("SELECT * FROM `sezony` WHERE `ID` = ".$IdSezonu.";");
+        $reszta = $polaczenie->query("SELECT * FROM `sezony` WHERE `ID` != ".$IdSezonu.";");
+        $wiersz = $wybrany->fetch_assoc();
             $Nazwa = $wiersz['Data'];
+            $ID = $wiersz['ID'];
             $Zakonczony = $wiersz['Zakonczony'];
-            echo '<a href="index.php?id='.$ID.'">'.$Nazwa.'</a>';
-        }
+        echo '<div class="menuSez">';
+            echo '<a class="first" href="index.php?id='.$ID.'">'.$Nazwa.'</a>';
+            echo '<div class="menuContent">';
+            for($i=0;$i<$reszta->num_rows;$i++){
+                $wiersz = $reszta->fetch_assoc();
+                $ID = $wiersz['ID'];
+                $Nazwa = $wiersz['Data'];
+                $Zakonczony = $wiersz['Zakonczony'];
+                echo '<a href="index.php?id='.$ID.'">'.$Nazwa.'</a>';
+            }
+    }else{
+        $ileSez = $polaczenie->query("SELECT * FROM `sezony` WHERE `Zakonczony` != 0;");
+        $aktywny = $polaczenie->query("SELECT * FROM `sezony` WHERE `Zakonczony` = 0;");
+        $wiersz = $aktywny->fetch_assoc();
+            $Nazwa = $wiersz['Data'];
+            $ID = $wiersz['ID'];
+            $Zakonczony = $wiersz['Zakonczony'];
+        echo '<div class="menuSez">';
+            echo '<a class="first" href="index.php?id='.$ID.'">'.$Nazwa.'</a>';
+            echo '<div class="menuContent">';
+            for($i=0;$i<$ileSez->num_rows;$i++){
+                $wiersz = $ileSez->fetch_assoc();
+                $ID = $wiersz['ID'];
+                $Nazwa = $wiersz['Data'];
+                $Zakonczony = $wiersz['Zakonczony'];
+                echo '<a href="index.php?id='.$ID.'">'.$Nazwa.'</a>';
+            }
+    }
         echo '</div>';
     echo '</div>';
-
 }
