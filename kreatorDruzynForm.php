f9cefd4d1e23c557951ad2ab27f9427e11f5e985
camzu1998 <kamillanger4@gmail.com>
2017-12-17 20:37:35 +0100

    Update For Team Creator

--- kreatorDruzynForm.php
+++ kreatorDruzynForm.php
@@ -1,24 +1,15 @@
 <?php
 $IDD = $_GET['druzyny'];
-$zawodnicy = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `ID_Druzyny` = '".$IDD."';");
-if($zawodnicy->num_rows != 0){
-    for($i=0;$i<$zawodnicy->num_rows;$i++){
-        $wiersz = $zawodnicy->fetch_assoc();
-            $ImieNazwisko = $wiersz['Imie Nazwisko'];
-    }
-}else{
-    function zawodnicy(){
-        $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `Plec`='M'");
-        for($i=0;$i<$rezultat->num_rows;$i++){
-            $wiersz = $rezultat->fetch_assoc();
-                $id = $wiersz['ID_zawodnika'];
-                $imieNazwisko = $wiersz['Imie Nazwisko'];
-                $idDruzyny = $wiersz['ID_druzyny'];
-            $rezultat2 = $polaczenie->query("SELECT * FROM `druzyny` WHERE `ID_druzyny`='".$idDruzyny."';");
-            $wiersz2 = $rezultat2->fetch_assoc();
-                $nazwa = $wiersz2['NazwaDruzyny'];
-            echo '<option value="'.$id.'">'.$imieNazwisko.'('.$nazwa.')</option>';
-        }
+$konkursRezultat = $polaczenie->query("SELECT * FROM `druzyny` WHERE `ID_druzyny`='".$IDD."';");
+$wiersz = $konkursRezultat->fetch_assoc();
+    $konkurs = $wiersz['konkurs'];
+function zawodnicy($rezultat){
+    for($i=0;$i<$rezultat->num_rows;$i++){
+        $wiersz = $rezultat->fetch_assoc();
+            $id = $wiersz['ID_zawodnika'];
+            $imieNazwisko = $wiersz['Imie Nazwisko'];
+            $idDruzyny = $wiersz['ID_druzyny'];
+        echo '<option value="'.$id.'">'.$imieNazwisko.'</option>';
     }
 }
 ?>
@@ -26,98 +17,68 @@ if($zawodnicy->num_rows != 0){
 <select name="zawodnik1" id="zawodnik1" class="Zawodnicy" required>
     <option style="display: none;"></option>
 <?php
-    $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `Plec`='M'");
-    for($i=0;$i<$rezultat->num_rows;$i++){
-        $wiersz = $rezultat->fetch_assoc();
-            $id = $wiersz['ID_zawodnika'];
-            $imieNazwisko = $wiersz['Imie Nazwisko'];
-            $idDruzyny = $wiersz['ID_druzyny'];
-        $rezultat2 = $polaczenie->query("SELECT * FROM `druzyny` WHERE `ID_druzyny`='".$idDruzyny."';");
-        $wiersz2 = $rezultat2->fetch_assoc();
-            $nazwa = $wiersz2['NazwaDruzyny'];
-        echo '<option value="'.$id.'">'.$imieNazwisko.'('.$nazwa.')</option>';
+    if($konkurs == 1){
+        $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `Plec`='M'");
+    }else{
+        $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE 1");
     }
+    zawodnicy($rezultat);
 ?>
 </select>
 <select name="zawodnik2" id="zawodnik2" class="Zawodnicy" required>
     <option style="display: none;"></option>
 <?php
-    $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `Plec`='M'");
-    for($i=0;$i<$rezultat->num_rows;$i++){
-        $wiersz = $rezultat->fetch_assoc();
-            $id = $wiersz['ID_zawodnika'];
-            $imieNazwisko = $wiersz['Imie Nazwisko'];
-            $idDruzyny = $wiersz['ID_druzyny'];
-        $rezultat2 = $polaczenie->query("SELECT * FROM `druzyny` WHERE `ID_druzyny`='".$idDruzyny."';");
-        $wiersz2 = $rezultat2->fetch_assoc();
-            $nazwa = $wiersz2['NazwaDruzyny'];
-        echo '<option value="'.$id.'">'.$imieNazwisko.'('.$nazwa.')</option>';
+    if($konkurs == 1){
+        $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `Plec`='M'");
+    }else{
+        $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE 1");
     }
+    zawodnicy($rezultat);
 ?>
 </select>
 <select name="zawodnik3" id="zawodnik3" class="Zawodnicy" required>
     <option style="display: none;"></option>
 <?php
-    $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `Plec`='M'");
-    for($i=0;$i<$rezultat->num_rows;$i++){
-        $wiersz = $rezultat->fetch_assoc();
-            $id = $wiersz['ID_zawodnika'];
-            $imieNazwisko = $wiersz['Imie Nazwisko'];
-            $idDruzyny = $wiersz['ID_druzyny'];
-        $rezultat2 = $polaczenie->query("SELECT * FROM `druzyny` WHERE `ID_druzyny`='".$idDruzyny."';");
-        $wiersz2 = $rezultat2->fetch_assoc();
-            $nazwa = $wiersz2['NazwaDruzyny'];
-        echo '<option value="'.$id.'">'.$imieNazwisko.'('.$nazwa.')</option>';
+    if($konkurs == 1){
+        $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `Plec`='M'");
+    }else{
+        $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE 1");
     }
+    zawodnicy($rezultat);
 ?>
 </select>
 <br>
 <select name="zawodnik4" id="zawodnik4" class="Zawodnicy" required>
     <option style="display: none;"></option>
 <?php
-    $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `Plec`='K'");
-    for($i=0;$i<$rezultat->num_rows;$i++){
-        $wiersz = $rezultat->fetch_assoc();
-            $id = $wiersz['ID_zawodnika'];
-            $imieNazwisko = $wiersz['Imie Nazwisko'];
-            $idDruzyny = $wiersz['ID_druzyny'];
-        $rezultat2 = $polaczenie->query("SELECT * FROM `druzyny` WHERE `ID_druzyny`='".$idDruzyny."';");
-        $wiersz2 = $rezultat2->fetch_assoc();
-            $nazwa = $wiersz2['NazwaDruzyny'];
-        echo '<option value="'.$id.'">'.$imieNazwisko.'('.$nazwa.')</option>';
+    if($konkurs == 1){
+        $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `Plec`='K'");
+    }else{
+        $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE 1");
     }
+    zawodnicy($rezultat);
 ?>
 </select>
 <select name="zawodnik5" id="zawodnik5" class="Zawodnicy" required>
     <option style="display: none;"></option>
 <?php
-    $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `Plec`='K'");
-    for($i=0;$i<$rezultat->num_rows;$i++){
-        $wiersz = $rezultat->fetch_assoc();
-            $id = $wiersz['ID_zawodnika'];
-            $imieNazwisko = $wiersz['Imie Nazwisko'];
-            $idDruzyny = $wiersz['ID_druzyny'];
-        $rezultat2 = $polaczenie->query("SELECT * FROM `druzyny` WHERE `ID_druzyny`='".$idDruzyny."';");
-        $wiersz2 = $rezultat2->fetch_assoc();
-            $nazwa = $wiersz2['NazwaDruzyny'];
-        echo '<option value="'.$id.'">'.$imieNazwisko.'('.$nazwa.')</option>';
+    if($konkurs == 1){
+        $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `Plec`='K'");
+    }else{
+        $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE 1");
     }
+    zawodnicy($rezultat);
 ?>
 </select>
 <select name="zawodnik6" id="zawodnik6" class="Zawodnicy" required>
     <option style="display: none;"></option>
 <?php
-    $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `Plec`='K'");
-    for($i=0;$i<$rezultat->num_rows;$i++){
-        $wiersz = $rezultat->fetch_assoc();
-            $id = $wiersz['ID_zawodnika'];
-            $imieNazwisko = $wiersz['Imie Nazwisko'];
-            $idDruzyny = $wiersz['ID_druzyny'];
-        $rezultat2 = $polaczenie->query("SELECT * FROM `druzyny` WHERE `ID_druzyny`='".$idDruzyny."';");
-        $wiersz2 = $rezultat2->fetch_assoc();
-            $nazwa = $wiersz2['NazwaDruzyny'];
-        echo '<option value="'.$id.'">'.$imieNazwisko.'('.$nazwa.')</option>';
+    if($konkurs == 1){
+        $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE `Plec`='K'");
+    }else{
+        $rezultat = $polaczenie->query("SELECT * FROM `zawodnicy` WHERE 1");
     }
+    zawodnicy($rezultat);
 ?>
 </select>
 <br>
